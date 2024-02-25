a157d29706198a93caf5171a640ca889bfe7853e
Devan <sdevadathan@protonmail.com>
2024-02-25 11:31:24 +0530

    More animation related changes

--- SurvivorsClone_Base-main/Survivors Clone/Player/player.gd
+++ SurvivorsClone_Base-main/Survivors Clone/Player/player.gd
@@ -2,13 +2,28 @@ extends CharacterBody2D
 
 
 var movement_speed = 50.0
-
-func _physics_process(delta):
+@onready var sprite = get_node("Sprite2D")
+@onready var walkTimer = get_node("%WalkTimer")
+func _physics_process(_delta):
 	movement()
 
 func movement():
 	var x_movement = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	var y_movement = Input.get_action_strength("Down") - Input.get_action_strength("Up")
 	var mov = Vector2(x_movement, y_movement)
+
+	if mov.x > 0:
+		sprite.flip_h = false
+	elif mov.x < 0:
+		sprite.flip_h = true
+
+	if mov != Vector2.ZERO:
+		if walkTimer.is_stopped():
+			if sprite.frame >= sprite.hframes - 1:
+				sprite.frame = 0
+			else:
+				sprite.frame += 1
+			walkTimer.start()
+
 	velocity = mov.normalized() * movement_speed
 	move_and_slide()
--- SurvivorsClone_Base-main/Survivors Clone/enemy.gd
+++ SurvivorsClone_Base-main/Survivors Clone/enemy.gd
@@ -1,11 +1,20 @@
 extends CharacterBody2D
 
-@export var movement_speed = 20.0
+@export var movement_speed = 25.0
+@onready var player = get_tree().get_first_node_in_group("Player")
+@onready var sprite = $Enemy2D
+@onready var animation = $AnimationPlayer
 
-@onready var player = get_tree().get_first_node_in_group("player")
+func _ready():
+	animation.play("walk")
 
 func _physics_process(_delta):
-	if player:
-		var direction = global_position.direction_to(player.global_position)
-		velocity = direction * movement_speed
-		move_and_slide()
+	var direction = global_position.direction_to(player.global_position)
+	print(direction)
+	velocity = direction * movement_speed
+	move_and_slide()
+
+	if direction.x > 0.1:
+		sprite.flip_h = true
+	elif direction.x < -0.1:
+		sprite.flip_h = false
